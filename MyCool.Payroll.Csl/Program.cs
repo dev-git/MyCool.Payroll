e6689d2fe978883e7e8d2df2c6c2d45477299c0e
Paul Bates <Paul.Bates@fidelitylife.co.nz>
2018-08-20 15:31:37 +1200

    Added new projects

--- MyCool.Payroll.Csl/Program.cs
+++ MyCool.Payroll.Csl/Program.cs
@@ -10,6 +10,8 @@ namespace MyCool.Payroll.Csl
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a key...");
+            Console.ReadLine();
         }
     }
 }
